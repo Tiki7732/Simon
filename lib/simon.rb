7cebbdcd57b7dd24036013f12a614a45d3391b60
tiki7732 <lsaidah@gmail.com>
2020-06-22 12:58:55 -0400

    take_turn and require_seq method

--- lib/simon.rb
+++ lib/simon.rb
@@ -20,6 +20,8 @@ class Simon
 
   def take_turn
     show_sequence
+    sleep(5)
+    system('clear')
     require_sequence
     unless game_over
       round_success_message
@@ -28,28 +30,37 @@ class Simon
   end
 
   def show_sequence
-
+    add_random_color
+    seq.each {|color| print color + " "}
+    print "\n"
   end
 
   def require_sequence
-
+    p "Now it's your turn!"
+    user_seq = gets.chomp.split
+    @game_over = true if user_seq != seq
   end
 
   def add_random_color
-    color_options = ["green", "yellow", "red", "blue"]
-    sequence_length.times {seq.push(color_options.sample)}
+
+    sequence_length.times {seq.push(COLORS.sample)}
 
   end
 
   def round_success_message
-
+    p "Moving on to next method"
   end
 
   def game_over_message
-
+    p "Game over!"
   end
 
   def reset_game
-
+    @sequence_length = 1
+    @seq = []
+    @game_over = false
   end
 end
+
+# simon = Simon.new
+# simon.play
